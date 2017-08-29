90f68ca30075ae3a1820b7b0fc03574b22acf0f3
ben harrington <harringtonben@icloud.com>
2017-08-29 12:07:32 -0500

    finished the class coding

--- main.js
+++ main.js
@@ -1,17 +1,7 @@
-console.log("hello");
-var dogArray = [];
-var breedsArray = [];
-
 function dogsJSONConvert() {
-	var data = JSON.parse(this.responseText);
-	dogArray = data.dogs;
-	console.log(dogArray);
-}
-
-function breedsJSONConvert() {
-	var data = JSON.parse(this.responseText);
-	breedsArray = data.breeds;
-	console.log(breedsArray);
+	var dogData = JSON.parse(this.responseText).dogs;
+	getBreedz(dogData);
+	// console.log('dogs data', dogData);
 }
 
 function executeThisCodeIfFileErrors() {
@@ -24,8 +14,48 @@ dogRequest.addEventListener("error", executeThisCodeIfFileErrors);
 dogRequest.open("GET", "dogs.json");
 dogRequest.send();
 
+function getBreedz(dogz) {
+// console.log('dogs array insidegetBreedz',dogz);
 var breedRequest = new XMLHttpRequest();
-breedRequest.addEventListener("load", breedsJSONConvert);
+breedRequest.addEventListener("load", breedsJSONConvert);	
 breedRequest.addEventListener("error", executeThisCodeIfFileErrors);
 breedRequest.open("GET", "breeds.json");
 breedRequest.send();
+
+	function breedsJSONConvert() {
+		var breedsData = JSON.parse(this.responseText).breeds;
+		// console.log('breeds data', breedsData);
+		// console.log('breeds', breedsData);
+		// console.log('dogs next to breeds data', dogz);
+		combinedArray(dogz, breedsData);
+	}
+}
+
+function combinedArray(dogsArray, breedsArray) {
+	// console.log('breeds array', breedsArray);
+	// console.log('dogs array', dogsArray);
+	dogsArray.forEach(function(dog){
+		var currentBreedID = dog['breed-id'];
+		// console.log(currentBreedID);
+		breedsArray.forEach(function(breed) {
+		if (currentBreedID === breed.id) {
+			// console.log(breed);
+			dog['dogBreed'] = breed.name;
+			dog['basePrice'] = breed['base-price'];
+			dog['description'] = breed.description;
+			dog['finalPrice'] = dog['add-on-price'] + dog.basePrice; 
+		}
+		});
+		// console.log(dog);
+	});
+	// console.log(dogsArray);
+	domString(dogsArray);
+}
+
+function domString(dogs) {
+
+}
+
+function writeToDom() {
+//write to dom
+}
